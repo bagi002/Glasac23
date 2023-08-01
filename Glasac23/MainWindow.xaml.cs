944992d2596e8f6017395e68fd89f32853f5c793
bagi002 <blagojemilosevic123@gmail.com>
2023-08-01 09:31:03 +0200

    Add project files.

---
+++ Glasac23/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Glasac23.pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Glasac23
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ucitavanjeMenija();
+        }
+
+        public void ucitavanjeMenija()
+        {
+
+            MainMenu meni = new MainMenu(ref Okvir);
+            Okvir.NavigationService.Navigate(meni);
+
+        }
+    }
+}
---
+++ Glasac23/pages/MainMenu.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Glasac23.pages
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+
+        Frame Okvir;
+        public MainMenu(ref Frame frejm)
+        {
+            InitializeComponent();
+            Okvir = frejm;
+        }
+
+        private void kreatorIzbora_Click(object sender, RoutedEventArgs e)
+        {
+
+          Kreiranje kreator = new Kreiranje();
+          Okvir.NavigationService.Navigate(kreator);
+
+        }
+    }
+}
